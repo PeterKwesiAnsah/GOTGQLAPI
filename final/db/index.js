973fad542a6e010d29aaffa4d830b1f06ac2180a
Ansah1234 <ansahpeter123@yahoo.com>
2021-03-22 20:31:09 +0100

    populating characters

--- final/db/index.js
+++ final/db/index.js
@@ -1,5 +1,7 @@
 import low from 'lowdb'
 import {FileSync} from 'lowdb/adapters'
  
-const adapter = new FileSync('db.json')
+const adapter = new FileSync('characters.json')
 const db = low(adapter)
+
+e
